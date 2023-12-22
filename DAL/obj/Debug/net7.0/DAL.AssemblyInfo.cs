2ab83191b8a0531f8e754c415ddfaddd0ea28078
hayq123 <phamquangha20012004@gmail.com>
2023-12-22 14:49:30 +0700

    hihi
    sa

--- DAL/obj/Debug/net7.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net7.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5298d0a84d2257f092b0bb523f6b589efb66377")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
