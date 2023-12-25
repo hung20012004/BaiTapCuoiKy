483bbf287e0584ca7971ebdb16773c40f810df62
hung20012004 <hamanhhung20012004@gmail.com>
2023-12-26 00:01:56 +0700

    new2

--- GUI/ManagerGUI_menu.cs
+++ GUI/ManagerGUI_menu.cs
@@ -51,6 +51,40 @@ namespace GUI
         {
             tabControl1.SelectedIndex = 1;
         }
+<<<<<<< HEAD
+=======
+        private string createUsername()
+        {
+            string username = "";
+            string str = tbHoTen.Text;
+            DateTime dateTime = DateTime.Now;
+            for (int i = str.Length - 1; i > -1; i--)
+            {
+                if (str[i] == ' ')
+                {
+                    str = str.Remove(0, i + 1);
+                    break;
+                }
+            }
+            username = utf8Convert.Convert(str) + dateTime.Minute + dateTime.Second + dateTime.Hour;
+            return username;
+        }
+        public string createPassword()
+        {
+            char[] password = new char[8];
+            string charSet = "";
+            System.Random random = new Random();
+            charSet += "abcdefghijklmnopqursuvwxyz";
+            charSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            charSet += "123456789";
+            charSet += @"!@£$%^&*()#€";
+            for (int i = 0; i < 8; i++)
+            {
+                password[i] = charSet[random.Next(charSet.Length - 1)];
+            }
+            return string.Join(null, password);
+        }
+>>>>>>> parent of be05ec6 (23/12)
         #endregion
 
         #region ClickEvent
@@ -84,7 +118,105 @@ namespace GUI
                 tbMatKhau.Text = Convert.ToString(row.Cells["Column4"].Value);
                 cbRole.Text = Convert.ToString(row.Cells["Column5"].Value);
             }
+<<<<<<< HEAD
 
+=======
+        }
+        private void btnThem1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Enabled = false;
+            tbHoTen.Enabled = true;
+            cbRole.Enabled = true;
+            btnGhi1.Enabled = true;
+            btnHuy1.Enabled = true;
+            btnSua1.Enabled = false;
+            btnXoa1.Enabled = false;
+            btnThem1.Enabled = false;
+            tbID.Text = "";
+            tbHoTen.Text = "";
+            tbTaiKhoan.Text = "********";
+            tbMatKhau.Text = "********";
+            cbRole.Text = "";
+        }
+        private void btnSua1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Enabled = false;
+            tbHoTen.Enabled = true;
+            cbRole.Enabled = true;
+            tbTaiKhoan.Enabled = true;
+            tbMatKhau.Enabled = true;
+            btnGhi1.Enabled = true;
+            btnHuy1.Enabled = true;
+            btnSua1.Enabled = false;
+            btnXoa1.Enabled = false;
+            btnThem1.Enabled = false;
+            
+        }
+        private void btnXoa1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Xác nhận xóa", "Thông báo", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                if (StaffBUS.Instance.delete(staff))
+                {
+                    tab0Reset();
+                    MessageBox.Show("Đã xóa thành công!");
+                }
+                else
+                {
+                    MessageBox.Show("Xóa không thành công!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Xóa không thành công!");
+            }
+        }
+        private void btnGhi1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Xác nhận ghi", "Thông báo", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                if (btnThem1.Enabled == true)
+                {
+                    if (tbHoTen.Text != "" && cbRole.Text != "")
+                    {
+                        tbMatKhau.Text = createPassword();
+                        tbTaiKhoan.Text = createUsername();
+                        if (StaffBUS.Instance.insert(staff))
+                        {
+                            tab0Reset();
+                            MessageBox.Show("Đã ghi thành công!");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Ghi không thành công!");
+                        }
+                    }
+                }
+                else
+                {
+                    if (StaffBUS.Instance.update(staff))
+                    {
+                        tab0Reset();
+                        MessageBox.Show("Đã ghi thành công!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ghi không thành công!");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Xóa không thành công!");
+            }
+        }
+        private void btnHuy1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Xác nhận hủy", "Thông báo", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                tab0Reset();
+            }
+>>>>>>> parent of be05ec6 (23/12)
         }
         #endregion
 
