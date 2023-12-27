f308939bc17a7840506be59bebf62ba560bca418
hung20012004 <hamanhhung20012004@gmail.com>
2023-12-27 10:56:23 +0700

    update dto

--- DTO/Provider.cs
+++ DTO/Provider.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace DTO
 {
-    internal class Provider
+    public class Provider
     {
+        private int id;
+        public int ID { 
+            get { return id; } 
+            set { id = value; }
+        }
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private string address;
+        public string Address
+        {
+            get { return address; }
+            set {  address = value; }
+        }
+        private string phone;
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
     }
 }
--- DTO/ServiceRequest.cs
+++ DTO/ServiceRequest.cs
@@ -6,7 +6,58 @@ using System.Threading.Tasks;
 
 namespace DTO
 {
-    internal class ServiceRequest
+    public class ServiceRequest
     {
+        private List<Laptop> laptops = new List<Laptop>();
+        public List<Laptop> Laptops
+        {
+            get { return laptops; }
+            set { laptops = value; }
+        }
+        private Staff warehouseKeeper = new Staff();
+        public Staff WarehouseKeeper
+        {
+            get { return warehouseKeeper; }
+            set {  warehouseKeeper = value; }
+        }
+        private int id;
+        public int ID
+        {
+            get { return id; }
+            set {  id = value; }
+        }
+        private Provider provider=new Provider();
+        public Provider Provider
+        {
+            get { return provider; }
+            set { provider = value; }
+        }
+        private DateTime importDate;
+        public DateTime ImportDate
+        {
+            get { return importDate; } 
+            set {  importDate = value; }
+        }
+        private int statusINT;
+        public int StatusINT
+        {
+            get { return statusINT; }
+            set { statusINT = value; }
+        }
+        private string statusSTRING;
+        public string StatusSTRING
+        {
+            get { return statusSTRING; }
+            set { statusSTRING = value; }
+        }
+        private void getStatusString()
+        {
+
+        }
+        private void getStatusInt() 
+        { 
+        
+        }
+
     }
 }
