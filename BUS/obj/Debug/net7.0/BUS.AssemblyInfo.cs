b7471a0320da2999fde80998298f3fc4155fae31
hayq123 <phamquangha20012004@gmail.com>
2023-12-30 17:29:56 +0700

    up

--- BUS/obj/Debug/net7.0/BUS.AssemblyInfo.cs
+++ BUS/obj/Debug/net7.0/BUS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BUS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28655c9f1dfe5b5dbd7cf046402239bbe2aaf77f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d732ae84bab7ab764515f9960a45a4f1b11dcf03")]
 [assembly: System.Reflection.AssemblyProductAttribute("BUS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BUS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
