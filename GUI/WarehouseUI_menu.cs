8801453c83c72c0cdcf4d7196c1c86ff9edc0c95
hung20012004 <hamanhhung20012004@gmail.com>
2024-01-01 15:41:43 +0700

    1/1

--- GUI/WarehouseUI_menu.cs
+++ GUI/WarehouseUI_menu.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using BUS;
+using DAL;
 using DTO;
 
 namespace GUI
@@ -15,6 +16,8 @@ namespace GUI
     public partial class WarehouseUI_menu : Form
     {
         private Staff user = new();
+        private ImportInvoice ImportInvoice = new ImportInvoice();
+        private List<Laptop> laptops = new List<Laptop>();
 
         public WarehouseUI_menu(Staff user)
         {
@@ -26,7 +29,10 @@ namespace GUI
         {
             tab0loading();
         }
-
+        private void btTaoHoaDonNhap_Click(object sender, EventArgs e)
+        {
+            tab2Loading();
+        }
         void tab0loading()
         {
             btGhi.Enabled = false;
@@ -42,6 +48,7 @@ namespace GUI
                 dataGridView1.Rows.Add(item.ID, item.Name, item.Address, item.Phone);
             }
             DataGridViewRow row = dataGridView1.Rows[0];
+
             if (Convert.ToString(row.Cells["Column1"].Value) != "")
             {
                 tbID.Text = Convert.ToString(row.Cells["Column1"].Value);
@@ -50,11 +57,58 @@ namespace GUI
                 tbPhone.Text = Convert.ToString(row.Cells["Column4"].Value);
             }
         }
+        void tab2Loading()
+        {
+            tabControl1.SelectedIndex = 2;
+            btnGhi2.Enabled = false;
+            btnHoanThanh.Enabled = false;
+            btnHuy2.Enabled = false;
+            btnHuyHoaDon.Enabled = false;
+            btnSua2.Enabled = false;
+            btnThem2.Enabled = false;
+            tbSoluong2.Enabled = false;
+            cbLaptop2.Enabled = false;
+            btnXoa2.Enabled = false;
+            btnTaoHoaDon.Enabled = true;
+            cbNhaCungCap.Enabled = true;
+
+            cbNhaCungCap.Items.Clear();
+            foreach (Provider item in ProviderDAL.Instance.get())
+            {
+                cbNhaCungCap.Items.Add(item.Name);
+            }
+            laptops.Clear();
+            laptops = LaptopDAL.Instance.get();
+            cbLaptop2.Items.Clear();
+            foreach (Laptop item in laptops)
+            {
+                cbLaptop2.Items.Add(item.Name);
+            }
+        }
         #endregion
         #region ClickEvent
         private void btNhaCungCap_Click(object sender, EventArgs e)
         {
             tab0loading();
+        }
+        private void btnTaoHoaDon_Click(object sender, EventArgs e)
+        {
+            if (cbNhaCungCap.Text != "")
+            {
+                btnXoa2.Enabled = true;
+                btnHoanThanh.Enabled = true;
+                btnHuyHoaDon.Enabled = true;
+                btnSua2.Enabled = true;
+                btnThem2.Enabled = true;
+                btnTaoHoaDon.Enabled = false;
+                cbNhaCungCap.Enabled = false;
+                cbLaptop2.Enabled = true;
+                tbSoluong2.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn nhà cung cấp!");
+            }
         }
         #endregion
         #region textChangeEvent
@@ -86,10 +140,27 @@ namespace GUI
             }
 
         }
-
         private void btSanPham_Click(object sender, EventArgs e)
         {
             //   tab1loading();
+        }
+
+        private void btnThem2_Click(object sender, EventArgs e)
+        {
+            if (cbLaptop2.Text != "" && tbSoluong2.Text != "")
+            {
+
+                dataGridView2.Rows.Add(tbLaptopID2, tbLaptop_Name, tbSoluong2);
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập đủ thông tin!");
+            }
+        }
+
+        private void cbLaptop2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
